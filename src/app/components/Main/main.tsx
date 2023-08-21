86c9679a6b74ad427e52d92d75543c5ff726ec41
ekoerkhe <ekoerkhe@ekoerkhes-MacBook-Pro.local>
2023-08-21 15:51:53 +0800

    test

---
+++ src/app/components/Main/main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="grid md:grid-cols-2 gap-4 sm:grid-cols-1">
+      <div className="bg-black h-60">1</div>
+      <div className="bg-black h-60">1</div>
+      <div className="bg-black h-60">1</div>
+      <div className="bg-black h-60">1</div>
+    </div>
+  );
+};
+
+export default Main;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import Main from "./components/Main/main";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Welcome to Ekoland</h1>
-    </main>
+    <div className="container mx-auto p-10">
+      <Main />
+    </div>
   );
 }
