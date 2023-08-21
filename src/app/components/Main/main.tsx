51afdbe06b63fe8f901befe38de2f75eb382e041
ekoerkhe <ekoerkhe@ekoerkhes-MacBook-Pro.local>
2023-08-21 16:47:49 +0800

    cat litter

--- src/app/components/Main/main.tsx
+++ src/app/components/Main/main.tsx
@@ -2,11 +2,67 @@ import React from "react";
 
 const Main = () => {
   return (
-    <div className="grid md:grid-cols-2 gap-4 sm:grid-cols-1">
-      <div className="bg-black h-60">1</div>
-      <div className="bg-black h-60">1</div>
-      <div className="bg-black h-60">1</div>
-      <div className="bg-black h-60">1</div>
+    <div className="grid lg:grid-cols-5 gap-4 md:grid-cols-3 sm:grid-cols-2 max-sm:grid-cols-1">
+      <div style={{ background: "#0079FF" }} className="h-80 rounded-md">
+        <div className="flex justify-center">
+          <img
+            src="https://scontent.xx.fbcdn.net/v/t1.15752-9/368234354_5744752915628308_3896885153856703055_n.png?_nc_cat=101&ccb=1-7&_nc_sid=aee45a&_nc_ohc=_3n6EYX5hEgAX9FUIiS&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdS3MP94w53JuA_yVg5yoi2Wa4xy6EDKS6UHJ_CngQxLLQ&oe=650A8B1B"
+            alt=""
+            width={165}
+          />
+        </div>
+        <div className="text-center pt-3">
+          <h1 className="text-white font-bold text-2xl">19,900₮</h1>
+        </div>
+      </div>
+      <div style={{ background: "#D71313" }} className="h-80 rounded-md">
+        <div className="flex justify-center">
+          <img
+            src="https://scontent.xx.fbcdn.net/v/t1.15752-9/368234354_5744752915628308_3896885153856703055_n.png?_nc_cat=101&ccb=1-7&_nc_sid=aee45a&_nc_ohc=_3n6EYX5hEgAX9FUIiS&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdS3MP94w53JuA_yVg5yoi2Wa4xy6EDKS6UHJ_CngQxLLQ&oe=650A8B1B"
+            alt=""
+            width={165}
+          />
+        </div>
+        <div className="text-center pt-3">
+          <h1 className="text-white font-bold text-2xl">19,900₮</h1>
+        </div>
+      </div>
+      <div style={{ background: "#F8DE22" }} className="h-80 rounded-md">
+        <div className="flex justify-center">
+          <img
+            src="https://scontent.xx.fbcdn.net/v/t1.15752-9/368234354_5744752915628308_3896885153856703055_n.png?_nc_cat=101&ccb=1-7&_nc_sid=aee45a&_nc_ohc=_3n6EYX5hEgAX9FUIiS&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdS3MP94w53JuA_yVg5yoi2Wa4xy6EDKS6UHJ_CngQxLLQ&oe=650A8B1B"
+            alt=""
+            width={165}
+          />
+        </div>
+        <div className="text-center pt-3">
+          <h1 className="text-white font-bold text-2xl">19,900₮</h1>
+        </div>
+      </div>
+      <div style={{ background: "#F94C10" }} className="h-80 rounded-md">
+        <div className="flex justify-center">
+          <img
+            src="https://scontent.xx.fbcdn.net/v/t1.15752-9/368234354_5744752915628308_3896885153856703055_n.png?_nc_cat=101&ccb=1-7&_nc_sid=aee45a&_nc_ohc=_3n6EYX5hEgAX9FUIiS&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdS3MP94w53JuA_yVg5yoi2Wa4xy6EDKS6UHJ_CngQxLLQ&oe=650A8B1B"
+            alt=""
+            width={165}
+          />
+        </div>
+        <div className="text-center pt-3">
+          <h1 className="text-white font-bold text-2xl">19,900₮</h1>
+        </div>
+      </div>
+      <div style={{ background: "#00DFA2" }} className="h-80 rounded-md">
+        <div className="flex justify-center">
+          <img
+            src="https://scontent.xx.fbcdn.net/v/t1.15752-9/368234354_5744752915628308_3896885153856703055_n.png?_nc_cat=101&ccb=1-7&_nc_sid=aee45a&_nc_ohc=_3n6EYX5hEgAX9FUIiS&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdS3MP94w53JuA_yVg5yoi2Wa4xy6EDKS6UHJ_CngQxLLQ&oe=650A8B1B"
+            alt=""
+            width={165}
+          />
+        </div>
+        <div className="text-center pt-3">
+          <h1 className="text-white font-bold text-2xl">19,900₮</h1>
+        </div>
+      </div>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Main from "./components/Main/main";
 export default function Home() {
   return (
-    <div className="container mx-auto p-10">
+    <div className="container mx-auto p-5">
       <Main />
     </div>
   );
